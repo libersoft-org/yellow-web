b4a36ee8c20b2b4079ff16da142c837380d598cc
gf87 <g.frkal@gmail.com>
2025-05-03 20:21:06 +0200

    Update structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,30 +1,14 @@
 <script>
-import Button from '@/theme/Button/Button.svelte';
-import Card from '@/theme/Card/Card.svelte';
+import Hero from '@/components/Hero/Hero.svelte';
 import AppFeatures from '@/components/AppFeatures/AppFeatures.svelte';
 import AppCharacteristics from '@/components/AppCharacteristics/AppCharacteristics.svelte';
+import Header from '@/components/Header/Header.svelte';
 </script>
 
-<div class="">
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-	<div class="container mx-auto">
-		<Button label="button" />
-		<Button theme="secondary" label="button" />
-
-		<div class="">
-			<h1 class="theme-text-h1">Title ok but <br /> <span class="theme-title-underline">with underline</span></h1>
-			<h1 class="theme-text-h2">Title ok but <br /> <span class="theme-title-underline">with underline</span></h1>
-			<h1 class="theme-text-h3">Title ok but <br /> <span class="theme-title-underline">with underline</span></h1>
-		</div>
-	</div>
-
-	<Card title="tetasd" icon="download">
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore.
-	</Card>
-
+<div>
+	<Header position="fixed" width="theme-container" />
+	<Hero />
 	<AppFeatures />
-
 	<AppCharacteristics />
 </div>
 
