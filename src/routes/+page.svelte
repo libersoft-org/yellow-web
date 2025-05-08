299e619320e3e95c563586f16f078ae0b4189808
gf87 <g.frkal@gmail.com>
2025-05-08 22:22:17 +0200

    Update layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script>
 import Header from '@/components/Header/Header.svelte';
+import Footer from '@/components/Footer/Footer.svelte';
 import Hero from '@/components/Hero/Hero.svelte';
 import AppFeatures from '@/components/AppFeatures/AppFeatures.svelte';
 import AppCharacteristics from '@/components/AppCharacteristics/AppCharacteristics.svelte';
 import AppFeaturesTable from '@/components/AppFeaturesTable/AppFeaturesTable.svelte';
 import AppFeaturesComparisonTable from '@/components/AppFeaturesComparisonTable/AppFeaturesComparisonTable.svelte';
+import TextImage from '@/components/TextImage/TextImage.svelte';
+import Text from '@/components/Text/Text.svelte';
+import FAQ from '@/components/FAQ/FAQ.svelte';
 </script>
 
 <div>
@@ -14,6 +18,10 @@ import AppFeaturesComparisonTable from '@/components/AppFeaturesComparisonTable/
 	<AppCharacteristics />
 	<AppFeaturesTable />
 	<AppFeaturesComparisonTable />
+	<TextImage />
+	<Text />
+	<FAQ />
+	<Footer />
 </div>
 
 <style>
