65fb5e7b4d2ab36ab8b47221e56ef9fffb31c90f
gf87 <g.frkal@gmail.com>
2025-05-25 16:12:45 +0200

    Update styling of textImage block

--- src/components/TextImage/TextImage.svelte
+++ src/components/TextImage/TextImage.svelte
@@ -19,20 +19,22 @@
 <div class="theme-container m-auto mb-12">
   <div
     class={[
-      'text-image-wrapper rounded-3xl px-7.5 py-5 md:px-15 md:py-7 lg:px-20 lg:py-10',
+      'text-image-wrapper rounded-3xl px-7.5 py-5 md:px-15 md:py-7 lg:py-10 lg:pr-4 lg:pl-20',
       'flex flex-col items-center justify-between md:flex-row',
-      imagePosition === 'left' ? 'flex-col-reverse md:flex-row-reverse' : ''
+      imagePosition === 'left' ? 'flex-col-reverse md:flex-row-reverse' : '',
+      'theme-gradient-radial-grey'
     ]}
-    style="background: radial-gradient(ellipse at 50% 50%, #F8F9FF 0%, #EAECF7 100%);"
   >
     <!-- Text Content -->
-    <div class="mb-8 flex-1 md:mr-8 md:mb-0">
+    <div class="mb-8 flex-1 md:mb-0">
       <h2 class="theme-text-h3 mb-4 font-bold">{title}</h2>
-      <p class="theme-text-body1 text-themeGray-800">{textContent}</p>
+      <p class="theme-text-body1 text-themeGray-800 font-medium">{textContent}</p>
     </div>
     <!-- Image Content -->
-    <div class="flex flex-1 justify-center">
-      <img alt={imageAlt} class="h-auto max-w-full rounded-xl" src={imagePath} />
+    <div class="relative flex h-full w-full flex-1 justify-center">
+      <div class="top-50% absolute left-0 translate-y-[-50%]">
+        <img alt={imageAlt} class="h-auto max-w-full rounded-xl" src={imagePath} />
+      </div>
     </div>
   </div>
 </div>
